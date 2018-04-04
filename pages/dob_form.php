9afdccee4d063f325525042077adca367173cd8c
matthewleighton <matthewrleighton@gmail.com>
2018-04-04 16:51:05 +0100

    Add submit addow to dob form

--- pages/dob_form.php
+++ pages/dob_form.php
@@ -1,8 +1,10 @@
-<div id="dob_form">
-	<p class="dob_instruction">Enter your date of birth</p>
+<div id="dob-form">
+	<p class="dob-instruction">Enter your date of birth</p>
 	<form method="POST">
-		<input type="date" name="dob" value="1993-04-17" id="dob_input">
-		<br/>
-		<input type="submit" id="dob_submit">
+		<input type="date" name="dob" value="1993-04-17" id="dob-input">
+		<button type="submit" id="dob-submit-button">
+			<img src="assets/images/right-arrow.svg" id="dob-submit-image"/>
+		</button>
+		<!--<input type="submit" id="dob-submit">-->
 	</form>
 </div>

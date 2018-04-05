4fa837b795de0df587e4ae2cd874cd88bd0b04ba
matthewleighton <matthewrleighton@gmail.com>
2018-04-05 15:59:01 +0100

    Add app explanation on homepage

--- pages/dob_form.php
+++ pages/dob_form.php
@@ -1,5 +1,11 @@
 <div id="dob-form">
+	
+	<p id="timeline-explained">
+		Overlay events in others' lives over the timeline of your own life.
+	</p>
+
 	<p class="dob-instruction">Enter your date of birth</p>
+
 	<form>
 		<input type="date" name="dob" value="1993-04-17" id="dob-input">
 		<button type="submit" id="dob-submit-button">
